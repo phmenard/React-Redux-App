c3df803fb88c697c2cbbbc3965a9c364568be59b
unknown <clmonitor2013@gmail.com>
2020-06-24 04:11:46 -0400

    structure

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -11,5 +11,17 @@ export const changeArtist = (song) => {
         }
     };
 
+    export const getSongs = (artist) => dispatch => {
+        dispatch({ type: "FETCHING_SONGS" });
+        axios
+          .get(`https://www.songsterr.com/a/ra/songs.json?pattern=${artist}`)
+          .then(res => {
+            console.log(res);
+            dispatch({ type: "FETCHING_SONG_SUCCESS", payload: res });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
     
 };
