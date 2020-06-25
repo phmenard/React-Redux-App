d55fea901c7730ecefcbe3cfb698e60cf29650ba
unknown <clmonitor2013@gmail.com>
2020-06-25 19:17:12 -0400

    added footer

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Artist from "./components/Artist";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import "./css/index.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Artist />
+      <Footer />
     </div>
   );
 }
